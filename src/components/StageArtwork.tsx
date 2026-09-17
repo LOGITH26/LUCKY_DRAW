95195f1492e6829173a7c75f9c29c08123bbc281
LOGITH26 <223021@student.upm.edu.my>
2026-09-18 00:15:59 +0800

    Updated StageArtwork.tsx

--- src/components/StageArtwork.tsx
+++ src/components/StageArtwork.tsx
@@ -8,25 +8,32 @@ export default function StageArtwork({ fullscreen }: StageArtworkProps) {
       className="stage-artwork pointer-events-none fixed inset-0 z-0 overflow-hidden"
       aria-hidden="true"
     >
+      {/* Left decorative artwork */}
       <img
         src="/assets/images/for_left_side.png"
         alt=""
-        className={`pointer-events-none absolute bottom-0 left-0 select-none stage-artwork-left ${
-          fullscreen ? 'stage-artwork-fullscreen' : ''
+        className={`pointer-events-none absolute bottom-0 left-0 select-none max-w-[200px] lg:max-w-[280px] object-contain stage-artwork-left ${
+          fullscreen ? 'stage-artwork-fullscreen max-w-[260px] lg:max-w-[340px]' : ''
         }`}
       />
+
+      {/* Right decorative artwork */}
       <img
         src="/assets/images/for_right_side.png"
         alt=""
-        className={`pointer-events-none absolute bottom-0 right-0 select-none stage-artwork-right ${
-          fullscreen ? 'stage-artwork-fullscreen' : ''
+        className={`pointer-events-none absolute bottom-0 right-0 select-none max-w-[200px] lg:max-w-[280px] object-contain stage-artwork-right ${
+          fullscreen ? 'stage-artwork-fullscreen max-w-[260px] lg:max-w-[340px]' : ''
         }`}
       />
+
+      {/* Center organizer badge */}
       <img
         src="/assets/images/organized_by_jbma.png"
         alt=""
-        className={`pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 select-none stage-artwork-center ${
-          fullscreen ? 'stage-artwork-fullscreen' : ''
+        className={`pointer-events-none absolute bottom-1 left-1/2 -translate-x-1/2 select-none object-contain stage-artwork-center ${
+          fullscreen
+            ? 'h-14 sm:h-18 md:h-22 max-w-[85vw] stage-artwork-fullscreen'
+            : 'h-8 sm:h-10 md:h-12 max-w-[300px]'
         }`}
       />
     </div>
