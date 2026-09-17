ef7dd940c5fb1537ca9838de4b7e0fcdc7ae67a2
LOGITH26 <223021@student.upm.edu.my>
2026-09-18 00:13:01 +0800

    Updated StageArtwork.tsx

--- src/components/StageArtwork.tsx
+++ src/components/StageArtwork.tsx
@@ -4,21 +4,30 @@ interface StageArtworkProps {
 
 export default function StageArtwork({ fullscreen }: StageArtworkProps) {
   return (
-    <div className="stage-artwork" aria-hidden="true">
+    <div
+      className="stage-artwork pointer-events-none fixed inset-0 z-0 overflow-hidden"
+      aria-hidden="true"
+    >
       <img
         src="/assets/images/for_left_side.png"
         alt=""
-        className={`stage-artwork-left ${fullscreen ? 'stage-artwork-fullscreen' : ''}`}
+        className={`pointer-events-none absolute bottom-0 left-0 select-none stage-artwork-left ${
+          fullscreen ? 'stage-artwork-fullscreen' : ''
+        }`}
       />
       <img
         src="/assets/images/for_right_side.png"
         alt=""
-        className={`stage-artwork-right ${fullscreen ? 'stage-artwork-fullscreen' : ''}`}
+        className={`pointer-events-none absolute bottom-0 right-0 select-none stage-artwork-right ${
+          fullscreen ? 'stage-artwork-fullscreen' : ''
+        }`}
       />
       <img
         src="/assets/images/organized_by_jbma.png"
         alt=""
-        className={`stage-artwork-center ${fullscreen ? 'stage-artwork-fullscreen' : ''}`}
+        className={`pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 select-none stage-artwork-center ${
+          fullscreen ? 'stage-artwork-fullscreen' : ''
+        }`}
       />
     </div>
   );
