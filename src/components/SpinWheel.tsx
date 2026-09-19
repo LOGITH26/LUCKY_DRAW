768d0024d97bfd8b93e6be57b0a9991ac77e70e4
LOGITH26 <223021@student.upm.edu.my>
2026-09-19 11:17:45 +0800

    Updated SpinWheel.tsx

--- src/components/SpinWheel.tsx
+++ src/components/SpinWheel.tsx
@@ -155,7 +155,7 @@ export default function SpinWheel({
 
  return (
     <div className="relative z-10 flex flex-col items-center justify-center w-full h-full">
-      <div className="relative z-10 w-full max-w-[min(83vh,800px)] aspect-square">
+      <div className="relative z-10 w-full max-w-[min(84vh,810px)] aspect-square">
         <canvas
           ref={canvasRef}
           onClick={handleClick}
